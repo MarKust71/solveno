e561d0abb24602c8ad8e7528906f505f1be418ae
Marek Kustosz <markust71@hotmail.com>
2020-12-04 23:58:48 +0100

    clean

--- client/src/endpoints/getAllFiles.ts
+++ client/src/endpoints/getAllFiles.ts
@@ -3,8 +3,6 @@ import api from './axios';
 export const getAllFiles = async () => {
     try {
         const filesList = await api.get('/');
-        // console.log('lista plików:', filesList.data);
-        // return await api.get('/');
         return filesList.data;
     } catch (error) {
         console.error('axios error:', error);
--- yarn-install.cmd
+++ yarn-install.cmd
@@ -1,1 +1,1 @@
-cd client && yarn install && cd ../server-node && yarn install && cd ../server-nest && yarn install && cd ..
+cd client && yarn install && cd ../server-nest && yarn install && cd ..
